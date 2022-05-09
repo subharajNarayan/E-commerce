de9b42fae5b0ff4da16c7747e1345732df15ede1
Subharaj Chaudhary <subharazzchy68@gmail.com>
2022-05-09 12:09:04 +0545

    update contact page

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 class Contact extends Component {
     render() {
         return (
-            <div>Contact</div>
+            <div>Contact Me Personally</div>
         )
     }
 }
